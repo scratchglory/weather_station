d53e4d2f0eb7ba7e5d7b1aceaf3caa6fd85ba0f4
scratchglory <48923305+scratchglory@users.noreply.github.com>
2019-06-25 21:39:50 -0400

    added gem files and worked on scraper file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source "https://rubygems.org"
 
+gem 'nokogiri', '~> 1.10', '>= 1.10.3'
+gem 'pry'
+gem 'rspec'
+
 
 # Specify your gem's dependencies in weather_station.gemspec
 gemspec
--- lib/weather_station.rb
+++ lib/weather_station.rb
@@ -1,4 +1,12 @@
-class WeatherStation 
+require 'nokogiri'
+require 'open-uri'
 
+class WeatherStation 
+    def scraper
+        url = "https://weather.com/"
+        doc = Nokogiri::HTML(oepn(url))
+    end
+
+scraper
     
 end
